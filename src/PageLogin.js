af19c711034b3ef4f7280602eda90e8716f3fe64
jenniferli1098 <jenniferli1098@gmail.com>
2020-09-19 16:37:43 -0400

    login

--- src/PageLogin.js
+++ src/PageLogin.js
@@ -1,7 +1,79 @@
 import React from 'react';
+import { firebaseConnect } from 'react-redux-firebase';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { Redirect } from 'react-router-dom';
+class PageLogin extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+        }
+    }
 
-function PageLogin() {
-  return <div>Login</div>;
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value});
+    }
+
+    login = async () => {
+        const credentials = {
+            email: this.state.email,
+            password: this.state.password,
+        };
+
+        try{
+            await this.props.firebase.login(credentials);
+        } catch(error) {
+            this.setState({error: error.message});
+        }
+    }
+    render () {
+        if (this.props.isLoggedIn) {
+          this.props.firebase.logout();
+        }
+        return (
+            
+            <div class="col-md-6 mx-auto">
+                <br></br>
+                <h1 class="text-center">Page Login</h1>
+                <br></br>
+                <div>{this.state.error}</div>
+                    <div class="form-group">
+                        
+                        <input
+                            name="email"
+                            class="form-control"
+                            onChange={this.handleChange}
+                            placeholder="Email"
+                            value={this.state.email}
+                        />
+                    </div>
+                    <div class="form-group">
+                        <input
+                            name="password"
+                            type="password"
+                            class="form-control"
+                            onChange={this.handleChange}
+                            placeholder="Password"
+                            value={this.state.password}
+                        />
+                    </div>
+
+                    <button class="btn btn-primary" onClick={this.login}>Login</button>
+                
+
+            </div>
+        )
+    }
 }
 
-export default PageLogin;
+const mapStateToProps = state => {
+    return {isLoggedIn: state.firebase.auth.uid};
+}
+
+export default compose(
+    firebaseConnect(),
+    connect(mapStateToProps),
+)
+(PageLogin);
--- src/PageRegister.js
+++ src/PageRegister.js
@@ -1,7 +1,107 @@
 import React from 'react';
+import { firebaseConnect } from 'react-redux-firebase';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { Redirect, Link } from 'react-router-dom';
 
-function PageRegister() {
-  return <div>Register</div>;
+class PageRegister extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      name: '',
+      phone: ''
+    };
+  }
+
+  handleChange = event =>
+    this.setState({ [event.target.name]: event.target.value, erroar: '' });
+
+  register = async () => {
+    const credentials = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+
+    const profile = {
+      name: this.state.name,
+      email: this.state.email,
+      phone: this.state.phone,
+      chat: []
+    };
+
+    try {
+      await this.props.firebase.createUser(credentials, profile);
+    } catch (error) {
+      this.setState({ error: error.message });
+    }
+  };
+
+  render() {
+    if (this.props.isLoggedIn) {
+      this.props.firebase.logout();
+    }
+
+    return (
+      
+        <div class="col-md-6 mx-auto">
+        <br></br>
+        <h1 class="text-center">Page Register</h1>
+        <br></br>
+            <div>{this.state.error}</div>
+            
+            <div class="form-group">
+                <input
+                    class="form-control"
+                    name="name"
+                    onChange={this.handleChange}
+                    placeholder="Name"
+                    value={this.state.name}
+                />
+            </div>
+            <div class="form-group">
+                <input
+                    name="email"
+                    class="form-control"
+                    onChange={this.handleChange}
+                    placeholder="Email"
+                    value={this.state.email}
+                />
+            </div>
+            <div class="form-group">
+                <input
+                    name="phone"
+                    class="form-control"
+                    onChange={this.handleChange}
+                    placeholder="Phone"
+                    value={this.state.phone}
+                />
+            </div>
+            <div class="form-group">
+                <input
+                    name="password"
+                    type="password"
+                    class="form-control"
+                    onChange={this.handleChange}
+                    placeholder="Password"
+                    value={this.state.password}
+                />
+            </div>
+
+            <button class="btn btn-primary" disabled={!this.state.name} onClick={this.register}>Register</button>
+        
+
+    </div>
+    );
+  }
 }
 
-export default PageRegister;
+const mapStateToProps = state => {
+  return { isLoggedIn: state.firebase.auth.uid };
+};
+
+export default compose(
+  firebaseConnect(),
+  connect(mapStateToProps),
+)(PageRegister);
