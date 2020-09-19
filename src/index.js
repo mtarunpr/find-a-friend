33399b0af0f9e754d930633ea4302667afe18b5f
Tarun Prasad <m.tarunprasad@gmail.com>
2020-09-20 01:29:42 +0530

    Bug fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import firebase from 'firebase/app';
 
 import { Provider } from 'react-redux';
 import firebase from 'firebase/app';
