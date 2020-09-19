f86c551334bc44be1ead3a19aa2912105ae81a6c
Tarun Prasad <m.tarunprasad@gmail.com>
2020-09-20 01:28:48 +0530

    Set up Firebase

--- src/index.js
+++ src/index.js
@@ -2,16 +2,57 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase/app';
+
+import { Provider } from 'react-redux';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import { createStore, combineReducers, compose } from 'redux';
+import {
+  ReactReduxFirebaseProvider,
+  firebaseReducer,
+} from 'react-redux-firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAMe7PAK5Zn0XoYWt3okgY_hiRcRkFvZxo',
+  authDomain: 'find-a-friend-hackmit.firebaseapp.com',
+  databaseURL: 'https://find-a-friend-hackmit.firebaseio.com',
+  projectId: 'find-a-friend-hackmit',
+  storageBucket: 'find-a-friend-hackmit.appspot.com',
+  messagingSenderId: '528000103219',
+  appId: '1:528000103219:web:bf3c5dbc6c8fc9e2590446',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// Add firebase to reducers
+const rootReducer = combineReducers({
+  firebase: firebaseReducer,
+  // firestore: firestoreReducer // <- needed if using firestore
+});
+
+// Create store with reducers and initial state
+const store = createStore(rootReducer);
+
+// react-redux-firebase config
+const rrfConfig = {
+  userProfile: 'users',
+  // useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+  // enableClaims: true // Get custom claims along with the profile
+};
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  // createFirestoreInstance // <- needed if using firestore
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
+  </Provider>,
+  document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
